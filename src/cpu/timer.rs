3b2973154671a0cd00b47a81071a5fb85aa4629e
Andy Russell <arussell123@gmail.com>
2017-12-13 14:02:23 -0500

    increment timer multiple times per tick if needed
    Fixes #48.

--- src/cpu/timer.rs
+++ src/cpu/timer.rs
@@ -21,8 +21,9 @@ impl TimerRegisters {
 #[derive(Debug, Default)]
 pub struct Timer {
     /// Divider internal counter. Increments the divider register once it reaches `64` M-cycles.
-    div: u8,
+    div_counter: u8,
 
+    // TODO: We might be able to just use `div_counter` for this.
     /// Timer internal counter.
     timer_counter: u16,
 
@@ -34,11 +35,11 @@ impl Timer {
     ///
     /// Returns whether the timer interrupt should be triggered.
     pub fn tick(&mut self, mtime: u8) -> bool {
-        self.div += mtime;
+        self.div_counter += mtime;
 
         // The divider is always counting, regardless of whether the timer is enabled.
-        if self.div >= 64 {
-            self.div %= 64;
+        while self.div_counter >= 64 {
+            self.div_counter -= 64;
             self.reg.divider = self.reg.divider.wrapping_add(1);
         }
 
@@ -57,8 +58,20 @@ impl Timer {
             _ => unreachable!(),
         };
 
-        if self.timer_counter >= threshold {
-            self.timer_counter %= threshold;
+        // NB: This is the source of a very common bug in timer implementations.
+        //
+        // Here, we need to increment the timer's internal counter relative to the tick size. The
+        // counter may have to be incremented multiple times for a given tick. While this
+        // technically could happen for the div internal counter, in practice it doesn't: no
+        // instruction takes longer to execute than it takes to increment DIV once. However, it
+        // _is_ possible to have the timer internal counter increment multiple times during a given
+        // instruction.
+        //
+        // Notably, getting this wrong will cause blargg's instr_timing test ROM to fail with
+        // the cryptic "Failure #255" message.
+        let mut timer_overflow = false;
+        while self.timer_counter >= threshold {
+            self.timer_counter -= threshold;
 
             let (counter, overflow) = match self.reg.counter.checked_add(1) {
                 Some(counter) => (counter, false),
@@ -66,15 +79,16 @@ impl Timer {
             };
 
             self.reg.counter = counter;
-            overflow
-        } else {
-            false
+            timer_overflow |= overflow;
         }
+
+        timer_overflow
     }
 
     pub fn reset_divider(&mut self) {
         self.reg.divider = 0;
-        self.div = 0;
+        self.div_counter = 0;
+        self.timer_counter = 0;
     }
 
     pub fn is_enabled(&self) -> bool {
@@ -84,6 +98,8 @@ impl Timer {
 
 #[cfg(test)]
 mod tests {
+    use std::u8;
+
     use super::Timer;
 
     #[test]
@@ -126,13 +142,41 @@ mod tests {
 
     #[test]
     fn tima() {
+        // Enable timer, increment every 64 M-cycles.
         let mut timer = Timer::default();
         timer.reg.control = 0x07;
 
-        for _ in 0..64 {
+        for _ in 0..63 {
             timer.tick(1);
         }
+        assert_eq!(timer.reg.counter, 0);
 
+        timer.tick(1);
         assert_eq!(timer.reg.counter, 1);
+
+        // Enable timer, increment every 4 M-cycles.
+        let mut timer = Timer::default();
+        timer.reg.control = 0x05;
+
+        timer.tick(16);
+        assert_eq!(timer.reg.counter, 4);
+    }
+
+    #[test]
+    fn tima_overflow() {
+        // Enable timer, increment every 4 M-cycles.
+        let mut timer = Timer::default();
+        timer.reg.control = 0x05;
+
+        // The number of M-cycles it will take to trigger an interrupt, divided by 8 iterations.
+        const INCREMENT: u16 = (u8::MAX as u16 * 4) / 8;
+
+        for _ in 0..8 {
+            let interrupt_requested = timer.tick(INCREMENT as u8);
+            assert!(!interrupt_requested);
+        }
+
+        let interrupt_requested = timer.tick(INCREMENT as u8);
+        assert!(interrupt_requested);
     }
 }
