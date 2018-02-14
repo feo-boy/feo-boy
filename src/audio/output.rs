a63acb598fe010f5fe44590358736d02a37144ba
Max Comstock <maxibillion6@gmail.com>
2018-02-14 00:57:05 -0500

    Beginnings of audio output

--- src/audio/output.rs
+++ src/audio/output.rs
@@ -3,3 +3,10 @@
 //! Plays the audio based on the state of the sound hardware.
 
 use audio::SoundController;
+
+/// Outputs the GameBoy audio for a given sound controller.
+#[derive(Debug, Default)]
+pub struct AudioOutput {
+    /// The sound controller which determines the audio to output.
+    sound_controller: SoundController,
+}
