73205a330b5f8c0b499a5ff73276072161945c47
Andy Russell <arussell123@gmail.com>
2020-12-10 18:13:11 -0500

    move execution to emulator method

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,18 @@ pub mod tui;
 
 use std::collections::HashSet;
 use std::process;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use anyhow::Result;
 use log::*;
+use pixels::{Pixels, SurfaceTexture};
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
 
 use crate::audio::SoundController;
 use crate::bus::Bus;
@@ -94,6 +100,95 @@ impl Emulator {
         info!("loaded ROM successfully");
 
         Ok(())
+    }
+
+    /// Open a graphical window and start execution of the emulator.
+    pub fn run(mut self) -> Result<()> {
+        let event_loop = EventLoop::new();
+        let mut input = WinitInputHelper::new();
+        let window = {
+            let size = LogicalSize::new(SCREEN_DIMENSIONS.0, SCREEN_DIMENSIONS.1);
+            WindowBuilder::new()
+                .with_title("FeO Boy")
+                .with_inner_size(size)
+                .with_min_inner_size(size)
+                .build(&event_loop)
+                .unwrap()
+        };
+        let mut hidpi_factor = window.scale_factor();
+
+        let mut pixels = {
+            let window_size = window.inner_size();
+            let surface_texture =
+                SurfaceTexture::new(window_size.width, window_size.height, &window);
+            Pixels::new(SCREEN_DIMENSIONS.0, SCREEN_DIMENSIONS.1, surface_texture)?
+        };
+
+        let mut last_update = Instant::now();
+
+        event_loop.run(move |event, _, control_flow| {
+            if let Event::RedrawRequested(_) = event {
+                self.render(pixels.get_frame());
+
+                if let Err(e) = pixels.render() {
+                    *control_flow = ControlFlow::Exit;
+                    error!("unable to render: {}", e);
+                    return;
+                }
+            }
+
+            if input.update(&event) {
+                if input.quit() {
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+
+                self.handle_keys(&input);
+
+                if let Some(factor) = input.scale_factor_changed() {
+                    hidpi_factor = factor;
+                }
+
+                if let Some(size) = input.window_resized() {
+                    // FIXME: User-specified scaling is currently ignored: parasyte/pixels/issues/89
+                    pixels.resize(size.width, size.height);
+                }
+
+                let current_time = Instant::now();
+                if let Err(e) = self.update(current_time - last_update) {
+                    error!("unable to update emulator state: {}", e);
+                    *control_flow = ControlFlow::Exit;
+                }
+                last_update = current_time;
+                window.request_redraw();
+            }
+        });
+    }
+
+    fn handle_keys(&mut self, input: &WinitInputHelper) {
+        macro_rules! button_mapping {
+            ( $( $winit_key:expr => $feo_boy_key:expr),+ $(,)? ) => {{
+                $(
+                    if input.key_pressed($winit_key) {
+                        self.press($feo_boy_key)
+                    }
+                    if input.key_released($winit_key) {
+                        self.release($feo_boy_key)
+                    }
+                )*
+            }}
+        }
+
+        button_mapping! {
+            VirtualKeyCode::Up => Button::Up,
+            VirtualKeyCode::Down => Button::Down,
+            VirtualKeyCode::Left => Button::Left,
+            VirtualKeyCode::Right => Button::Right,
+            VirtualKeyCode::X => Button::B,
+            VirtualKeyCode::Z => Button::A,
+            VirtualKeyCode::Return => Button::Start,
+            VirtualKeyCode::Back => Button::Select,
+        }
     }
 
     /// Render the current frame into a frame buffer.
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
 use std::fs;
 use std::path::PathBuf;
 use std::process;
-use std::time::Instant;
 
 use anyhow::{Context, Result};
 use log::*;
-use pixels::{Pixels, SurfaceTexture};
 use structopt::clap::AppSettings::*;
 use structopt::StructOpt;
-use winit::dpi::LogicalSize;
-use winit::event::{Event, VirtualKeyCode};
-use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::WindowBuilder;
-use winit_input_helper::WinitInputHelper;
 
-use feo_boy::{Button, Emulator, SCREEN_DIMENSIONS};
+use feo_boy::Emulator;
 
 #[derive(Debug, StructOpt)]
 #[structopt(setting(ColorAuto), setting(ColoredHelp))]
@@ -59,91 +52,7 @@ fn run(opt: Opt) -> Result<()> {
 
     emulator.reset();
 
-    let event_loop = EventLoop::new();
-    let mut input = WinitInputHelper::new();
-    let window = {
-        let size = LogicalSize::new(SCREEN_DIMENSIONS.0, SCREEN_DIMENSIONS.1);
-        WindowBuilder::new()
-            .with_title("FeO Boy")
-            .with_inner_size(size)
-            .with_min_inner_size(size)
-            .build(&event_loop)
-            .unwrap()
-    };
-    let mut hidpi_factor = window.scale_factor();
-
-    let mut pixels = {
-        let window_size = window.inner_size();
-        let surface_texture =
-            SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(SCREEN_DIMENSIONS.0, SCREEN_DIMENSIONS.1, surface_texture)?
-    };
-
-    let mut last_update = Instant::now();
-
-    event_loop.run(move |event, _, control_flow| {
-        if let Event::RedrawRequested(_) = event {
-            emulator.render(pixels.get_frame());
-
-            if let Err(e) = pixels.render() {
-                *control_flow = ControlFlow::Exit;
-                error!("unable to render: {}", e);
-                return;
-            }
-        }
-
-        if input.update(&event) {
-            if input.quit() {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-
-            handle_keys(&input, &mut emulator);
-
-            if let Some(factor) = input.scale_factor_changed() {
-                hidpi_factor = factor;
-            }
-
-            if let Some(size) = input.window_resized() {
-                // FIXME: User-specified scaling is currently ignored: parasyte/pixels/issues/89
-                pixels.resize(size.width, size.height);
-            }
-
-            let current_time = Instant::now();
-            if let Err(e) = emulator.update(current_time - last_update) {
-                error!("unable to update emulator state: {}", e);
-                *control_flow = ControlFlow::Exit;
-            }
-            last_update = current_time;
-            window.request_redraw();
-        }
-    });
-}
-
-fn handle_keys(input: &WinitInputHelper, emulator: &mut Emulator) {
-    macro_rules! button_mapping {
-        ( $( $winit_key:expr => $feo_boy_key:expr),+ $(,)? ) => {{
-            $(
-                if input.key_pressed($winit_key) {
-                    emulator.press($feo_boy_key)
-                }
-                if input.key_released($winit_key) {
-                    emulator.release($feo_boy_key)
-                }
-            )*
-        }}
-    }
-
-    button_mapping! {
-        VirtualKeyCode::Up => Button::Up,
-        VirtualKeyCode::Down => Button::Down,
-        VirtualKeyCode::Left => Button::Left,
-        VirtualKeyCode::Right => Button::Right,
-        VirtualKeyCode::X => Button::B,
-        VirtualKeyCode::Z => Button::A,
-        VirtualKeyCode::Return => Button::Start,
-        VirtualKeyCode::Back => Button::Select,
-    }
+    emulator.run()
 }
 
 fn main() {
