1ac82059741450bb02939846bb3909a7a250093a
Andy Russell <arussell123@gmail.com>
2020-12-10 22:19:29 -0500

    do not panic on invalid cartridges

--- src/web.rs
+++ src/web.rs
@@ -7,30 +7,37 @@ use web_sys::HtmlElement;
 
 use crate::Emulator;
 
+fn handle_error(e: anyhow::Error) {
+    error!("fatal error: {}", e);
+
+    let document = web_sys::window()
+        .and_then(|window| window.document())
+        .unwrap();
+    document
+        .get_element_by_id("error-text")
+        .unwrap()
+        .set_inner_html(&e.to_string());
+    document
+        .get_element_by_id("modal")
+        .and_then(|element| element.dyn_into::<HtmlElement>().ok())
+        .unwrap()
+        .style()
+        .set_property("display", "block")
+        .unwrap();
+}
+
 #[wasm_bindgen]
 pub fn run_emulator(rom: &[u8]) {
     let mut emulator = Emulator::new();
 
-    emulator.load_rom(rom).unwrap();
+    if let Err(e) = emulator.load_rom(rom) {
+        handle_error(e);
+        return;
+    }
 
     wasm_bindgen_futures::spawn_local(async move {
         if let Err(e) = emulator.run().await {
-            error!("fatal error: {}", e);
-
-            let document = web_sys::window()
-                .and_then(|window| window.document())
-                .unwrap();
-            document
-                .get_element_by_id("error-text")
-                .unwrap()
-                .set_inner_html(&e.to_string());
-            document
-                .get_element_by_id("modal")
-                .and_then(|element| element.dyn_into::<HtmlElement>().ok())
-                .unwrap()
-                .style()
-                .set_property("display", "block")
-                .unwrap();
+            handle_error(e);
         }
     });
 }
